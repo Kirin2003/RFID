ae41c3c26f39d79a9496627e5dd358af38f4e604
hxq <1405871856@qq.com>
2022-08-08 22:01:10 +0800

    TagListGenerator类加上类别id

--- src/main/java/base/TagListGenerator.java
+++ src/main/java/base/TagListGenerator.java
@@ -12,50 +12,63 @@ public class TagListGenerator {
      *
      * @param tagIDLength length of tag id
      * @param allTagNum length of the original tag list
+     * @param cidLength length of category id
+     * @param density number of tag per category
      * @param unknownTagNum number of unknownTags tags
      * @param missingTagNum number of missing tags
      * @return TagRepository
      */
-    public static TagRepository generateTagRepository(int tagIDLength, int cidLength, int allTagNum, int unknownTagNum, int missingTagNum){
-        List<Tag> allTagList = TagListGenerator.tagListFactory(tagIDLength, cidLength, allTagNum);
+    public static TagRepository generateTagRepository(int tagIDLength, int cidLength, int allTagNum, int density, int unknownTagNum, int missingTagNum){
+        List<Tag> allTagList = TagListGenerator.tagListFactory(tagIDLength, cidLength, allTagNum,density);
         List<Tag> expectedTagList = TagListGenerator.removeTag(allTagList, unknownTagNum);
         List<Tag> actualTagList = TagListGenerator.removeTag(expectedTagList, missingTagNum);
         return new TagRepository(allTagList, expectedTagList, actualTagList);
     }
 
     /**
-     * Generates an expected tag list with the according to the tag ID length and tag number
+     * Generates an expected tag list with the according to the tag ID length, category id length, tag number, tag number per cid
      * @param tagIDLength Tag ID Length
+     * @param cidLength Category ID length
      * @param all Number of tags
+     * @param density number of tag per category
      * @return List of expected tags
      */
-    public static List<Tag> tagListFactory(int tagIDLength, int cidLength, int all) {
+    public static List<Tag> tagListFactory(int tagIDLength, int cidLength, int all, int density) {
         List<Tag> allTagList = new ArrayList<>();
         for (int i = 0; i < all; i++)
             allTagList.add(new Tag());
-        allocateRandom(allTagList, tagIDLength, cidLength);
+        allocateRandom(allTagList, tagIDLength, cidLength,density);
         return allTagList;
     }
 
+
+
     /**
-     * Sets a random tagID to each tag in the list according to the ID length
-     * @param tagList list of Tags
-     * @param tagIDLength Length of the tag ID
+     * Compared with allocateRandom, allocateRandom2 controls thr number of tags in each category equal to density
+     * @param tagList
+     * @param tagIDLength
+     * @param categoryIDLength
+     * @param density : the number of tags in each category
      */
-    public static void allocateRandom(List<Tag> tagList, int tagIDLength, int cidLength){
-        for (Tag tag: tagList) {
-            tag.setTagID(getRandomTagID(tagIDLength));
+    private static void allocateRandom(List<Tag> tagList, int tagIDLength, int categoryIDLength, int density) {
+        int num = 0;
+        String cid = getRandomID(categoryIDLength);
+        for (Tag tag : tagList) {
+            tag.setTagID(getRandomID(tagIDLength));
+            num ++;
+            tag.setCategoryID(cid);
+            if(num%density==0) cid=getRandomID(categoryIDLength);
         }
     }
 
     /**
      * Random Tag Id generator
-     * @param tagIDLength give length of the tag ID
+     * @param IDLength give length of the tag ID
      * @return the returned string can be used as a tagID
      */
-    private static String getRandomTagID(int tagIDLength){
+    private static String getRandomID(int IDLength){
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < tagIDLength; i++)
+        for (int i = 0; i < IDLength; i++)
             sb.append((int)(Math.random()>0.5?'1':'0'));
         return sb.toString();
     }
